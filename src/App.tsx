e15f2abe856b92c40dc6e26b0cd4b75d19fe86ec
thanhson3133 <thanhson3133@gmail.com>
2022-08-19 14:05:03 +0700

    source typescript

---
+++ src/App.tsx
@@ -0,0 +1,13 @@
+import React from "react";
+import Home from "./Home";
+import logo from "./logo.svg";
+
+function App() {
+  return (
+    <div>
+      <Home/>
+    </div>
+  );
+}
+
+export default App;
---
+++ src/Home/index.tsx
@@ -0,0 +1,13 @@
+import React from 'react'
+
+export default function Homeaa() {
+  return (
+    <div className="p-10 min-h-screen flex items-center justify-center bg-cool-gray-700">
+      <h1 className="text-9xl font-black text-white text-center">
+        <span className="bg-gradient-to-r text-transparent bg-clip-text from-green-400 to-purple-500">
+          Sơn
+        </span>
+      </h1>
+    </div>
+  )
+}
